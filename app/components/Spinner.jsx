aa8843d896b96aa4b68d79b460e24bf2f0166e59
Tony K Tan <tankwantiong@gmail.com>
2016-11-05 00:54:03 -0700

    (cleanup) lint Spinner.jsx

--- app/components/Spinner.jsx
+++ app/components/Spinner.jsx
@@ -1,24 +1,17 @@
 // Spinner.js
-import React from 'react';
-
 import {
   StyleSheet,
-  Text,
-  View,
-  ScrollView,
-  Image,
 } from 'react-native';
 
 import {
-  MKProgress,
   MKSpinner,
 } from 'react-native-material-kit';
 
 const styles = StyleSheet.create({
-spinner: {
+  spinner: {
     width: 40,
-    height: 40
-  }
+    height: 40,
+  },
 });
 
 const Spinner = MKSpinner.singleColorSpinner()
