b1ae34efd1fda95e942ebb82c830de3dacfde14d
Tony K Tan <tankwantiong@gmail.com>
2016-11-05 00:57:55 -0700

    (cleanup) lint TopBar.jsx

--- app/components/TopBar.jsx
+++ app/components/TopBar.jsx
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import {
   TouchableOpacity,
   StyleSheet,
@@ -6,33 +6,6 @@ import {
   View,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-
-export default class TopBar extends Component {
-  static propTypes = {
-    handleLeftPress: PropTypes.func.isRequired,
-    topBarFilterVisible: PropTypes.bool,
-    topBarName: PropTypes.string,
-    iconName: PropTypes.string
-  }
-
-  constructor(props) {
-    super(props);
-  }
-
-  render () {
-    return (
-      <View style={styles.container}>
-        <TouchableOpacity onPress={this.props.handleLeftPress}>
-          <Icon name={this.props.iconName || 'menu'} style={styles.content}></Icon>
-        </TouchableOpacity>
-        <Text style={styles.logo}>{this.props.topBarName || 'Sembly'}</Text>
-        <TouchableOpacity>
-          { this.props.topBarFilterVisible ? <Icon name='filter-list' style={styles.content}></Icon> : <Text></Text> }
-        </TouchableOpacity>
-      </View>
-    );
-  }
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -44,7 +17,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     backgroundColor: '#7924B8',
     alignItems: 'center',
-    alignSelf: 'stretch'
+    alignSelf: 'stretch',
   },
   content: {
     fontSize: 22,
@@ -53,6 +26,27 @@ const styles = StyleSheet.create({
   logo: {
     color: 'white',
     fontSize: 18,
-    fontWeight: 'bold'
-  }
+    fontWeight: 'bold',
+  },
 });
+
+const TopBar = props => (
+  <View style={styles.container}>
+    <TouchableOpacity onPress={props.handleLeftPress}>
+      <Icon name={props.iconName || 'menu'} style={styles.content} />
+    </TouchableOpacity>
+    <Text style={styles.logo}>{props.topBarName || 'Sembly'}</Text>
+    <TouchableOpacity>
+      { props.topBarFilterVisible ? <Icon name="filter-list" style={styles.content} /> : <Text /> }
+    </TouchableOpacity>
+  </View>
+);
+
+TopBar.propTypes = {
+  handleLeftPress: PropTypes.func.isRequired,
+  topBarFilterVisible: PropTypes.bool,
+  topBarName: PropTypes.string,
+  iconName: PropTypes.string,
+};
+
+export default TopBar;
